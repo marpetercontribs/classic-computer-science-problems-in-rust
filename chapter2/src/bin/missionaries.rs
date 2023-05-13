b3244b2362fea1c037c10805fb77e99826edf4a7
marpeter <marpeter@web.de>
2023-05-13 15:43:13 +0200

    fix some clippy warnings in ch2

--- chapter2/src/bin/missionaries.rs
+++ chapter2/src/bin/missionaries.rs
@@ -13,8 +13,6 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use generic_search;
-
 const MAX_NUM: usize = 3;
 
 #[derive(PartialEq, PartialOrd, Eq, Clone, Hash, Copy)]
