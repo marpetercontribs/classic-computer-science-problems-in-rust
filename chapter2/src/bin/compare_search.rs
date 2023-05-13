1b05a979d2081d8eb2109838ddfa9b818029fc06
marpeter <marpeter@web.de>
2023-05-13 15:43:13 +0200

    fix some clippy warnings in ch2

--- chapter2/src/bin/compare_search.rs
+++ chapter2/src/bin/compare_search.rs
@@ -14,7 +14,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-use generic_search;
+
 use std::time::Instant;
 
 fn main() {
