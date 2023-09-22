ae7341a7ee29ab201a90e655a03e4cb972d7a1d9
marpeter <marpeter@web.de>
2023-09-22 20:22:23 +0200

    completed chapter 7.3

--- chapter7/src/util.rs
+++ chapter7/src/util.rs
@@ -13,3 +13,17 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+use std::iter::zip;
+
+pub fn dot_product(xs: &[f64], ys: &[f64]) -> f64 {
+    zip(xs, ys).map(|(x, y)| x * y).sum::<f64>()
+}
+
+pub fn sigmoid(x: f64) -> f64 {
+    1.0 / (1.0 + (-x).exp())
+}
+
+pub fn sigmoid_derivative(x: f64) -> f64 {
+    let sigmoid = sigmoid(x);
+    sigmoid * (1.0 - sigmoid)
+}
